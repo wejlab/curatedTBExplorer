95bd5ee4c852dfad301b22baa6efcbb5515cd0c6
W. Evan Johnson <wevanjohnson@gmail.com>
2024-02-06 17:12:05 -0700

    Update server_01_visualization.R

--- inst/shiny/server/server_01_visualization.R
+++ inst/shiny/server/server_01_visualization.R
@@ -1,8 +1,8 @@
-source(file.path("../../R", "runTBsigProfilerFunction.R"), local = TRUE)$value
+# source(file.path("../../R", "runTBsigProfilerFunction.R"), local = TRUE)$value
 
 output$ssgsea_table <- renderDT({
   selected_dataset <- input$dataset
   selected_profiles <- c(input$profile1, input$profile2, input$profile3)
   selected_assay <- input$assay
-  runTBsigProfilerFunction(selected_dataset, selected_profiles, selected_assay)
+  curatedTBExplorer::runTBsigProfilerFunction(selected_dataset, selected_profiles, selected_assay)
 })
