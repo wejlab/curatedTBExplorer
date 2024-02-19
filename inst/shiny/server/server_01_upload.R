8a7e5d5f6462fd49d2d54fd36633a0d6f0256105
andrew <ajsajs02@gmail.com>
2024-02-19 16:15:30 -0700

    Added base download functionality, as well as selection/ deselection on data table

--- inst/shiny/server/server_01_upload.R
+++ inst/shiny/server/server_01_upload.R
@@ -1,5 +1,18 @@
-# Reactive values to store selected studies
-selected_studies <- reactiveVal(NULL) #might remove this block, it helped in the beginning but since changed the checkboxes to select-click it looks dumb
+#To Do:
+  #add multithreading for download
+  #output the downloads to the summary tab (don't know how yet - Andrew) - I think we need to save to rda file too
+  #add "default" dataset so summary can be viewed without downloading anything
+    #this may allow us to ignore the summary tab only appearing after continue button
+  #fix issue where summary tab should only appear once continue button is clicked
+  #fix issue regarding deselecting datasets - works sometimes
+  #Review and delete unneccessary code
+  #change the selected columns if statements to a function so it's prettier
+  #maybe add progress bar for downloads for user? or some pop up to let them know something is happening
+
+#reactive values for various tasks
+selected_studies <- reactiveVal(NULL) #this is used to update the selected studies accordingly
+continue_clicked <- reactiveVal(FALSE) #stores if Continue button is clicked
+
 
 #Grab the selected checkboxes from the ui section, and only output these within the datatable
 #super ugly block of code, but it works. Will make prettier later - Andrew
@@ -49,7 +62,8 @@ selected_columns <- reactive({
   return(selected_columns)
 })
 
-# Display study information table
+
+#Display study information table
 output$study_table <- renderDT({
 
   #these were added to grab the selected information from the checkboxes within the ui
@@ -78,8 +92,10 @@ output$study_table <- renderDT({
   )
 })
 
-# Display study description based on selected studies
 
+#Display study description based on selected studies
+#I don't think we need this anymore - Andrew
+#download is achieved in the following observeEvent block
 output$study_description <- renderPrint({
   if (!is.null(input$selected_study)) {
     capture.output({ #captures download status info
@@ -89,22 +105,39 @@ output$study_description <- renderPrint({
   }
 })
 
-# Reactive value to store whether Continue button is clicked
-continue_clicked <- reactiveVal(FALSE)
 
-# Observer to update reactive value when Continue button is clicked
+#updates if continue button clicked, also begins the download process for all selected studies
 observeEvent(input$continue, {
   continue_clicked(TRUE)
+  #if there are studies selected, this block executes
+  if (!is.null(selected_studies())) {
+    #lapply is used here
+    #NOTE: still need to apply multithreaded download, however the base download functionality is present
+    selected_studies_info <- lapply(selected_studies(), function(study_id) {
+      curatedTBData(study_id, dry.run = FALSE, curated.only = FALSE)
+    })
+    #not sure if we actually are going to need this, keeping for now
+    for (study_info in selected_studies_info) {
+      print(study_info)
+      View(study_info)
+    }
+  }
 })
 
-# Display the Summarize tab only if Continue button is clicked
+
+#Display the Summarize tab only if Continue button is clicked
+#this isn't working - Andrew
+#i dont think we need this, as if we have default data then the summarize tab should always be available
 observe({
   if (continue_clicked()) {
     updateTabsetPanel(session, "main", selected = "Summarize")
   }
 })
 
-# Render selected studies tables on the Summarize tab
+
+#render selected studies tables on the Summarize tab
+#not currently working - Andrew
+#believe we need to change the selected_studies to work with the selected_studies() as I did for download
 output$selected_studies_table <- renderDT({
   if (!is.null(input$selected_studies)) {
     data.frame(
@@ -114,9 +147,28 @@ output$selected_studies_table <- renderDT({
   }
 })
 
-# Observer to capture selected row(s) and update the description
-observe({
-  if (!is.null(input$selected_study)) {
-    selected_studies(input$selected_study)
+
+#added to ensure that every study that is clicked is added
+#deselection currently kind of works, except when you deselect the previously selected study
+#it stays in the selected_studies(), leading to unexpected behavior
+observeEvent(input$selected_study, {
+  current_selection <- isolate(input$selected_study)
+  current_studies <- selected_studies()
+  #uses the setdiff function to set the selected studies correctly
+  if (current_selection %in% current_studies) {
+    selected_studies(setdiff(current_studies, current_selection))
+  } else {
+    selected_studies(c(current_studies, current_selection))
   }
+  #allows you to see the selected studies in r studio
+  View(selected_studies())
 })
+
+
+#I don't think we need this anymore - Andrew
+# Observer to capture selected row(s) and update the description
+# observe({
+#   if (!is.null(input$selected_study)) {
+#     selected_studies <- c(selected_studies, input$selected_study)
+#   }
+# })
