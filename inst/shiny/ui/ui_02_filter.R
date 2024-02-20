bd659d758735f33e69c4310a60775c8850213181
pleaselukau <110324939+pleaselukau@users.noreply.github.com>
2024-02-20 14:58:25 -0700

    Added side panel block

--- inst/shiny/ui/ui_02_filter.R
+++ inst/shiny/ui/ui_02_filter.R
@@ -1,9 +1,37 @@
-tabPanel( #summarize tab empty for now
-  "Summarize",
-  icon = icon("table"),
-  fluidRow(
-    column(12,
-           DTOutput("selected_studies_table")
+tabPanel("Summarize",
+             tabsetPanel(
+               tabPanel("Filter",
+                        sidebarPanel(
+                          h4("Selected Studies:"),
+                          verbatimTextOutput("selected_studies_text"),
+                          
+                          selectInput("filter_by", "Filter By",
+                                      choices = c("TB Status", "HIV Status", "Diabetes Status")),
+                          
+                          conditionalPanel(
+                            condition = "input.filter_by == 'TB Status'",
+                            selectInput("tb_status", "TB Status", choices = c("PTB", "LTBI"))
+                          ),
+                          conditionalPanel(
+                            condition = "input.filter_by == 'HIV Status'",
+                            selectInput("hiv_status", "HIV Status", choices = c("Positive", "Negative"))
+                          ),
+                          conditionalPanel(
+                            condition = "input.filter_by == 'Diabetes Status'",
+                            selectInput("diabetes_status", "Diabetes Status", choices = c("Positive", "Negative"))
+                          ),
+                          
+                          actionButton("filter_button", "Filter"),
+                          actionButton("reset_button", "Reset")
+                        ),
+                        mainPanel(
+                          DTOutput("summary_table"),
+                          plotOutput("summary_plots")
+                        )
+               ),
+               
+               tabPanel("Categorize",
+                        # code for the "Categorize" sub-tab
+               )
+             )
     )
-  )
-)
