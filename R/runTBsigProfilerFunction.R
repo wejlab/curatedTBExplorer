0d40f8199faaee5f882e127f503c8407587aacf3
andrew <ajsajs02@gmail.com>
2024-03-05 14:48:40 -0700

    Updates to TBSig (still nonfunctional)

--- R/runTBsigProfilerFunction.R
+++ R/runTBsigProfilerFunction.R
@@ -15,55 +15,57 @@
 #'
 #' @export
 
+#derived from the TBSignatureProfiler Vignette
 runTBsigProfilerFunction <- function(selected_dataset, selected_profiles) {
   appDir <- system.file("shiny", package = "curatedTBExplorer")
   if (appDir == "") {
-    stop("Could not find my function. Try re-installing 'curatedTBExplorer'.",
-         call. = FALSE
-    )
+    stop("Could not find my function. Try re-installing 'curatedTBExplorer'.", call. = FALSE)
   }
 
+  #assays to be created
+  # selected_dataset <- mkAssay(selected_dataset, counts, log = TRUE, counts_to_CPM = TRUE)
 
-  #chooses the dataset
-  # if (selected_dataset == "TB_hiv") {
-  #   dataset_data <- mkAssay(TB_hiv, log = TRUE, counts_to_CPM = TRUE)
-  # } else if (selected_dataset == "TB_indian") {
-  #   dataset_data <- mkAssay(TB_indian, log = TRUE, counts_to_CPM = FALSE)
-  # } else {
-  #   stop("Invalid dataset selected.")
-  # }
+  #attempts to create the necessary assays, as mkAssay was having issues
+  #also not working currently
+  # log_counts_assay <- log(assay(selected_dataset))
+  # counts_CPM_assay <- assay(selected_dataset) / colSums(assay(selected_dataset)) * 1e6
+  # log_counts_CPM_assay <- log(counts_CPM_assay + 1)
+  #
+  # n_rows <- nrow(log_counts_assay)
+  # n_cols <- ncol(log_counts_assay)
+  #
+  # selected_dataset$assays <- SimpleList(
+  #   log_counts = matrix(log_counts_assay, nrow = n_rows, ncol = n_cols),
+  #   counts_CPM = matrix(counts_CPM_assay, nrow = n_rows, ncol = n_cols),
+  #   log_counts_CPM = matrix(log_counts_CPM_assay, nrow = n_rows, ncol = n_cols)
+  # )
+  # assays(selected_dataset) <- selected_dataset$assays
 
-  #arbitrarily set to false the counts to cpm
-  dataset_data <- mkAssay(selected_dataset, log = TRUE, counts_to_CPM = FALSE)
+  View(selected_dataset$assays)
 
-  #this runs the profiler: following is modified straight from TBSignatureProfiler vignette
+  #runs the tbsigprofiler
   out <- capture.output({
     ssgsea_result <- runTBsigProfiler(
-      input = dataset_data,
-      useAssay = "log_counts",
-      signatures = TBsignatures,
-      algorithm = "ssGSEA",
+      input = selected_dataset,
+      useAssay = NULL, #will need to change based on user input
+      signatures = TBsignatures, #may potentially need to change? though I don't think so
+      algorithm = "ssGSEA", #need to add user input to select algorithms
       combineSigAndAlgorithm = TRUE,
       parallel.sz = 1,
       update_genes = FALSE
     )
   })
 
-  #removes unscored signatures
+  #Removes unscored signatures
   TBsignatures <- subset(TBsignatures, !(names(TBsignatures) %in% c("Chendi_HIV_2")))
 
-  #info for the datatable
+  #Info for the datatable
   selected_sigs <- unlist(selected_profiles)
 
-  #this changes the columns dependent on the selected dataset
-  if(selected_dataset == "TB_hiv"){
-    column <- colData(ssgsea_result)[, c("Disease", selected_sigs)]
-  } else if(selected_dataset == "TB_indian"){
-    column <- colData(ssgsea_result)[, c("sample", selected_sigs)]
-  }
-
-  ssgsea_print_results <- as.data.frame( column )
+  #This changes the columns dependent on the selected dataset
+  ssgsea_print_results <- as.data.frame(colData(ssgsea_result)[, c("Disease", selected_sigs)])
   ssgsea_print_results[, 2:length(selected_sigs) + 1] <- round(ssgsea_print_results[, 2:length(selected_sigs) + 1], 4)
-  #create and outputs the datatable
+
+  #Create and output the datatable
   datatable(ssgsea_print_results)
 }
