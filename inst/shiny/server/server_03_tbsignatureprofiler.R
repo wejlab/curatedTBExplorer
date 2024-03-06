fff49e5857e902c45021c3f10c143a7420039d5b
andrew <ajsajs02@gmail.com>
2024-03-06 16:51:36 -0700

    Small Updates

--- inst/shiny/server/server_03_tbsignatureprofiler.R
+++ inst/shiny/server/server_03_tbsignatureprofiler.R
@@ -1,5 +1,12 @@
-#note: somtimes it just fails? but then will work otherwise - look into
 
+#adding functionality to select the profiles
+#also, to select which assays to run the profiler on
+#also, to select which algorithms to use (ssgsea, etc)
+# if(input$profile1) {
+#   input1 <- choices
+# }
+#
+# View(input1)
 
 observeEvent(input$begin, {
   # View(the$downloaded_datasets)
@@ -8,7 +15,6 @@ observeEvent(input$begin, {
   temp_se <- toSE(vals$MAEList[1])
   # View(temp_se)
   # se <- toSE(vals$MAEList[[1]])
-
   output$ssgsea_table <- renderDT({
     selected_dataset <- temp_se
     selected_profiles <- c(input$profile1, input$profile2, input$profile3)
--- inst/shiny/ui/ui_03_tbsignatureprofiler.R
+++ inst/shiny/ui/ui_03_tbsignatureprofiler.R
@@ -3,14 +3,12 @@
 tabPanel(
   "TBSigProfiler",
   sidebarPanel(
-    # dropdown to select the dataset - not sure we actually need this implemented, as i believe we will just use the downloaded datasets?
-    # selectInput("dataset", "Select Dataset:", choices = temp$downloaded_datasets),
     # dropdowns to select the profile information
     selectInput("profile1", "Select Profile 1:", choices = names(TBsignatures), selected = "Anderson_42"),
     selectInput("profile2", "Select Profile 2:", choices = names(TBsignatures), selected = "Anderson_OD_51"),
     selectInput("profile3", "Select Profile 3:", choices = names(TBsignatures), selected = "Gong_OD_4"),
     # dropdown to select assay
-    # not currently working
+    # not currently working implemente
     selectInput("assay", "Select Assay:", choices = c("log_counts", "log_counts_cpm", "counts"), selected = "log_counts")
   ),
   # titlePanel(""),
