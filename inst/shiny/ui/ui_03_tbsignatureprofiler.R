af5cd634a88c54bfddbc52afd1b38166afa7d956
Alevarias <alexjusticesierra@gmail.com>
2024-03-14 16:40:20 -0600

    compressed checkboxes with conditional panel

--- inst/shiny/ui/ui_03_tbsignatureprofiler.R
+++ inst/shiny/ui/ui_03_tbsignatureprofiler.R
@@ -5,15 +5,19 @@ tabPanel(
       "Profiler",
       sidebarPanel(
         "Select Data For the TBSignatureProfiler",
-        # dropdowns to select the profile information
-        checkboxGroupInput("profiles", "Select Profiles:",
-                           choices = names(TBsignatures),
-                           selected = names(TBsignatures)),
+        actionButton("showProfiles", "Profiler Selection"),
+        conditionalPanel(
+          condition = "input.showProfiles % 2 != 0",
+          # dropdowns to select the profile information
+          checkboxGroupInput("profiles", "Select Profiles:",
+                             choices = names(TBsignatures),
+                             selected = names(TBsignatures)),
 
-        # dropdown to select assay
-        selectInput("assay", "Select Assay:",
-                    choices = c("log_counts", "log_counts_cpm", "counts"),
-                    selected = "log_counts")
+          # dropdown to select assay
+          selectInput("assay", "Select Assay:",
+                      choices = c("log_counts", "log_counts_cpm", "counts"),
+                      selected = "log_counts")
+        )
       ),
       # titlePanel(""),
       DTOutput("ssgsea_table"),
@@ -33,8 +37,12 @@ tabPanel(
       "Boxplots",
       sidebarPanel(
         "Create Boxplots",
-        checkboxGroupInput("box_profiles", "Select Profiles:",
-                           choices = names(TBsignatures)),
+        actionButton("selectProfiles", "Profiler Selection"),
+        conditionalPanel(
+          condition = "input.selectProfiles % 2 != 0",
+          checkboxGroupInput("box_profiles", "Select Profiles:",
+                             choices = names(TBsignatures)),
+        )
       ),
       actionButton("genBoxplots", "Create Boxplots"),
       plotOutput("boxplot_result", height = "200")
