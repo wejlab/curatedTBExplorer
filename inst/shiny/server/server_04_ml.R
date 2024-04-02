843e995223fef26cf305d9ecae3c3edf498469a7
Alevarias <alexjusticesierra@gmail.com>
2024-04-02 14:59:02 -0600

    updated code

--- inst/shiny/server/server_04_ml.R
+++ inst/shiny/server/server_04_ml.R
@@ -1,1 +1,78 @@
-install.packages("randomForest")
+# Code For General Settings And Selecting Datasets
+rv <- reactiveValues(
+  # Holds the users' choices for outcomes
+  # Still need to figure out the format that this will come in
+  outcomeChoice1 = NULL,
+  outcomeChoice2 = NULL,
+
+  # Results of user choice
+  trainingSE = NULL,
+  testingSE = NULL
+)
+
+outcomeChoice1 <- reactive({
+  input$oc1
+})
+outcomeChoice2 <- reactive({
+  input$oc2
+})
+
+# Code for Random Forests
+observeEvent(input$continueRF, {
+  # Might need to check the SEList
+  # DE_analyze(vals$SEList, 'limma', "logCPM")
+  View(vals$SEList)
+
+})
+
+
+
+
+################# Couple of Questions #############################
+
+# In what format are we feeding in the conditions variable since it's said to be like PTB vs LTBI
+# is it just c("PTB", "LTBI") ?
+
+# Are we making each ML model have their own continue button or run them all at once?
+# WE ARE MAKING THEM SEPERATELY
+# This is especially important since each model has their own specific settings that need to be filled out seperately
+
+# Wait, what is batch? Prof Johnson says it's the study, but what if there are multiple?
+
+
+
+
+
+
+# Code for Support Vector Machines
+
+
+
+
+
+
+
+
+
+
+
+# Code for Elastic Net Regression
+
+
+
+
+
+
+
+
+# Code for Neural Networks
+
+
+
+
+
+
+
+
+
+
--- inst/shiny/ui/ui_04_ml.R
+++ inst/shiny/ui/ui_04_ml.R
@@ -4,10 +4,17 @@ tabPanel(
   fluidRow(
     column(width = 3,
            "General Settings",
-           selectInput("outputClass", "outputClass", c("LTBI", "PTB", "Control", "OD"))
+           style = "background-color: #EDF1F1;",
+           selectInput("oc1", "Outcome", choices = c("LTBI", "PTB", "Control", "OD")),
+           selectInput("oc2", "Compared Outcome", choices = c("LTBI", "PTB", "Control", "OD")),
+           numericInput("featureSelectionCount", "Feature Count", value = 500, min = 0, step = 1),
+           numericInput("foldCount", "Cross Validation Folds", value = 10, min = 0, step = 1)
            ),
     column(width = 9,
-           "Training & Testing")
+           "Training & Testing Dataset Selection",
+           DTOutput("trainingDatasetTable"),
+           DTOutput("testingDatasetTable")
+           )
   ),
   tabsetPanel(
     # Contains general settings for the machine learning
@@ -16,14 +23,16 @@ tabPanel(
       "Elastic Net Regression",
       sidebarPanel(
         "Elastic Net Regression"
-      )
+      ),
+      actionButton("continueEN", "Continue")
 
     ),
     tabPanel(
       "Neural Networks",
       sidebarPanel(
         "Neural Networks"
-      )
+      ),
+      actionButton("continueNN", "Continue")
     ),
 
     tabPanel(
@@ -31,14 +40,16 @@ tabPanel(
       sidebarPanel(
         "Random Forests"
 
-      )
+      ),
+      actionButton("continueRF", "Continue")
     ),
 
     tabPanel(
       "Support Vector Machines",
       sidebarPanel(
         "Support Vector Machines"
-      )
+      ),
+      actionButton("continueSVM", "Continue")
     )
   )
 )
